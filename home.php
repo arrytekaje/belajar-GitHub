fe29efbaeafe63126c5e272ef6911912ce3a947d
Ari Susanto <arrytekaje@gmail.com>
2020-09-06 23:26:41 +0700

    Membuat sidebar.php agar sidebar otomatis

--- home.php
+++ home.php
@@ -1,4 +1,5 @@
 <?php
+
 ?>
 
 
@@ -13,23 +14,15 @@
 <body>
     <div class="container clearfix">
         <h1>Belajar Git_PHP_Native</h1>
-        <div class="sidebar">
-            <ul>
-                <li><a href="#">Bekerja dengan Git</a></li>
-                <li><a href="#">Command_Git</a></li>
-                <li><a href="#">Git Branch & Merge</a></li>
-                <li><a href="#">Git Merge Conflict</a></li>
-                <li><a href="#">Git Remote</a></li>
-                <li><a href="#">GitHub Pages</a></li>
-                <li><a href="#">Multiple Remote</a></li>
-                <li><a href="#">Git Ignore</a></li>
-                <li><a href="#">Git Rebase</a></li>
-                <li><a href="#">Git & Web Hosting</a></li>
-            </ul>
-        </div>
+        
+        <?php
+            include 'sidebar.php';
+        ?>        
+        
         <div class="content">
             <h1>Selamat datang, <?php echo "admin :)";?></h1>
         </div>
+        
     </div>
 </body>
 
---
+++ sidebar.php
@@ -0,0 +1,14 @@
+<div class="sidebar">
+    <ul>
+        <li><a href="#">Bekerja dengan Git</a></li>
+        <li><a href="#">Command_Git</a></li>
+        <li><a href="#">Git Branch & Merge</a></li>
+        <li><a href="#">Git Merge Conflict</a></li>
+        <li><a href="#">Git Remote</a></li>
+        <li><a href="#">GitHub Pages</a></li>
+        <li><a href="#">Multiple Remote</a></li>
+        <li><a href="#">Git Ignore</a></li>
+        <li><a href="#">Git Rebase</a></li>
+        <li><a href="#">Git & Web Hosting</a></li>
+    </ul>
+</div>
