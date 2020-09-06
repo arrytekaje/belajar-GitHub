2c8a28a55beebe16f1561ee7259627900b07381d
Ari Susanto <arrytekaje@gmail.com>
2020-09-06 20:11:55 +0700

    Memodifikasi file home.php ; menghapus file sidebar.php ; mempersingkat codingan pada file home.php ; sudah itu aja si, yang lainnya belum ada lagi

--- home.php
+++ home.php
@@ -1,15 +1,32 @@
 <?php
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Selamat Datang!</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    Teks yang di tampilkan :
-    <h1> Hello World!</h1>
+    <div class="container clearfix">
+        <h1>Perpustakaan Lima5atu</h1>
+        <div class="sidebar">
+            <ul>
+                <li><a href="#">Data Kategori</a></li>
+                <li><a href="#">Data Buku</a></li>
+                <li><a href="#">Data Anggota</a></li>
+                <li><a href="#">Peminjaman</a></li>
+                <li><a href="#">Pengembalian</a></li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <h1>Selamat datang, <?php echo "admin :)";?></h1>
+        </div>
+    </div>
 </body>
 
 </html>
