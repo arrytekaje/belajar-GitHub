33ea2a78541659950d3f0c4b6ed6f0eb3e1b1fb1
Ari Susanto <arrytekaje@gmail.com>
2020-09-06 20:45:10 +0700

    Merubah file home.php dan membuat catatab command_Git

---
+++ Command_Git.php
@@ -0,0 +1,19 @@
+git init
+ 
+git status
+ 
+git commit
+ 
+git commit -m "Pesan yg ingin Anda tulis"
+ 
+git log
+ 
+git log -1 
+git log -2
+git log -...dst
+ 
+git log --all --decorate --oneline --graph 
+
+alias graph="git log --all --decorate --oneline --graph"
+
+alias sts="git status"
--- home.php
+++ home.php
@@ -15,7 +15,15 @@
         <h1>Belajar Git_PHP_Native</h1>
         <div class="sidebar">
             <ul>
-                <li><a href="#">Data Kategori</a></li>
+                <li><a href="#">Bekerja dengan Git</a></li>
+                <li><a href="#">Git Branch & Merge</a></li>
+                <li><a href="#">Git Merge Conflict</a></li>
+                <li><a href="#">Git Remote</a></li>
+                <li><a href="#">GitHub Pages</a></li>
+                <li><a href="#">Multiple Remote</a></li>
+                <li><a href="#">Git Ignore</a></li>
+                <li><a href="#">Git Rebase</a></li>
+                <li><a href="#">Git & Web Hosting</a></li>
             </ul>
         </div>
         <div class="content">
